19d574b4f4f8d5359174dd420410cd4d514c5a1b
Fabrice Braun <97706664+novix6806@users.noreply.github.com>
2026-07-17 10:09:17 +0200

    suite selenium web

---
+++ Selenium_Automatiser les tests/TP M4/TP Memory/test_accueil_example.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def test_accueil(driver):
+    driver.get("https://enijdenis.github.io/memory/index.html")
+    driver.find_element(By.LINK_TEXT, "Accueil").click()
+
+    WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "p"))
+    )
+
+    assert driver.find_element(By.CSS_SELECTOR, "p").text == "Bienvenue sur le site du Memory de l'ENI."
